ce6a0d08042780bfc5836af62effa72c8081aa63
adam krieger <adamkrieger87@gmail.com>
2015-08-25 10:41:49 -0400

    added .env for db and added simple css

--- resources/views/index.php
+++ resources/views/index.php
@@ -9,36 +9,11 @@
     <script src="/assets/js/app.js"></script>
 
     <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    <link href="/module/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="/modules/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="/assets/css/app.css" rel="stylesheet">
 
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            height: 100%;
-            background-color: #333;
-            /*display: table;*/
-            font-weight: 100;
-            font-family: 'Lato';
-        }
-        .title {
-            font-size: 96px;
-            margin-bottom: 40px;
-        }
-
-        .quote {
-            font-size: 24px;
-        }
-    </style>
 </head>
 <body ng-app="myApp">
     <div ng-view></div>
-
-<!--<div class="container">-->
-<!--    <div class="content">-->
-<!--        <div class="title">Lumen.</div>-->
-<!--    </div>-->
-<!--</div>-->
 </body>
 </html>
